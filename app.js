f6efeb33a252cd3cffdb8abd6e9df1b73c83dc4e
Aydin Elhan <aydinmate@protonmail.com>
2023-01-22 21:32:21 +1100

    First version that has been converted to ejs

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+
+
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+// Home Page
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+
+// About Page
+app.get("/about", function(req, res) {
+    res.render("about");
+});
+
+
+
+
+
+
+
+var port = 3000
+app.listen(port, function() {
+    console.log("Server is running on port " + port + ".");
+});
